7c042b725b26ceb036b6fd396e2318b5ccd395cb
pankajverma-1 <pankajverma1361861@gmail.com>
2022-12-13 14:11:36 +0530

    add responsive

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -14,41 +14,43 @@ const HomeScreen = () => {
 
   return (
     <>
-      <div className="text-center h1">QuadB Tech Assignment</div>
-      <div className="row container mt-5 ms-auto me-auto">
-        {data.map((item) => (
-          <div
-            className="col-12 col-md-6 col-lg-4 col-xxl-3 my-2"
-            key={item.show.id}
-          >
-            <div class="card" style={{ width: '18rem' }}>
-              <img
-                src={item.show.image.original}
-                class="card-img-top"
-                alt={item.show.id}
-                style={{ height: '400px' }}
-              />
-              <div class="card-body">
-                <h5 class="card-title">{item.show.name}</h5>
-                <p
-                  class="card-text"
-                  style={{
-                    maxWidth: '100%',
-                    display: '-webkit-box',
-                    WebkitBoxOrient: 'vertical',
-                    WebkitLineClamp: 3,
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                  }}
-                  dangerouslySetInnerHTML={{ __html: item.show.summary }}
+      <div className="d-flex flex-column align-items-center">
+        <div className="text-center h1">QuadB Tech Assignment</div>
+        <div className="row container my-5">
+          {data.map((item) => (
+            <div
+              className="col-12 col-md-6 col-lg-4 col-xxl-3 my-2 "
+              key={item.show.id}
+            >
+              <div className="card ms-auto me-auto" style={{ width: '18rem' }}>
+                <img
+                  src={item.show.image.original}
+                  className="card-img-top"
+                  alt={item.show.id}
+                  style={{ height: '400px' }}
                 />
-                <Link to={`/${item.show.id}`} class="btn btn-primary">
-                  Summary
-                </Link>
+                <div className="card-body">
+                  <h5 className="card-title">{item.show.name}</h5>
+                  <p
+                    className="card-text"
+                    style={{
+                      maxWidth: '100%',
+                      display: '-webkit-box',
+                      WebkitBoxOrient: 'vertical',
+                      WebkitLineClamp: 3,
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                    }}
+                    dangerouslySetInnerHTML={{ __html: item.show.summary }}
+                  />
+                  <Link to={`/${item.show.id}`} className="btn btn-primary">
+                    Summary
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </>
   );
--- src/Summery.jsx
+++ src/Summery.jsx
@@ -8,8 +8,6 @@ const Summery = () => {
     const allData = JSON.parse(localStorage.getItem('data'));
     const byId = allData.filter((item) => parseInt(_id) === item.show.id);
     setData(byId);
-    console.log(_id);
-    return () => {};
   }, [_id]);
 
   return (
@@ -17,10 +15,10 @@ const Summery = () => {
       <div className="text-center h1">Summary</div>
       <div className="row container mt-5 ms-auto me-auto">
         {data.map((item) => (
-          <div class="card w-100">
+          <div className="card w-100" key={item.show.id}>
             <img
               src={item.show.image.original}
-              class=" card-img"
+              className=" card-img"
               alt={item.show.id}
               style={{
                 width: '100%',
@@ -28,9 +26,11 @@ const Summery = () => {
                 objectFit: 'cover',
               }}
             />
-            <div class="card-body">
-              <h5 class="card-title">{item.show.name}</h5>
-              <h5 class="card-title text-opacity-25">{item.show.language}</h5>
+            <div className="card-body">
+              <h5 className="card-title">{item.show.name}</h5>
+              <h5 className="card-title text-opacity-25">
+                {item.show.language}
+              </h5>
               <a
                 href={item.show.url}
                 className="h4 my-2"
@@ -40,7 +40,7 @@ const Summery = () => {
                 Link
               </a>
               <p
-                class="card-text"
+                className="card-text"
                 dangerouslySetInnerHTML={{ __html: item.show.summary }}
               />
             </div>
